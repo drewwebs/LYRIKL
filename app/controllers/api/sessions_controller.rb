0811751d368178df3e16400784c17f7db42c9c9a
Drew <drewwebs@gmail.com>
2020-09-23 09:47:26 -0400

    Error styling and adjustments complete

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -7,8 +7,13 @@ class Api::SessionsController < ApplicationController
         if @user
             login!(@user)
             render 'api/users/show'
-        else   
-            render json: ['Invalid credentials'], status: 401
+        else
+            @user = User.find_by_username(params[:user][:name]) || User.find_by_email(params[:user][:name])
+            if @user
+                render json: ['Invalid password'], status: 401
+            else
+                render json: ['Invalid nickname or email'], status: 401
+            end
         end
     end
 
