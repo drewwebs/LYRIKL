74ad347813722bfe21de1ef43949e48197f82de1
Drew <drewwebs@gmail.com>
2020-09-24 18:21:06 -0400

    Song annotations added

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,76 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(username: 'Demo', email: 'Demo', password: 'hunter12')
+
+songs = Song.create([
+    {
+        title: "Crank Dat", 
+        artist: "Soulja Boy", 
+        lyrics: , 
+        primary_tag: "Hip-hop"
+    },
+
+    {
+        title: "Exile", 
+        artist: "Taylor Swift", 
+        lyrics: , 
+        primary_tag: "Pop" 
+    },
+
+    {
+        title: "Lost in the Supermarket", 
+        artist: "The Clash", 
+        lyrics: , 
+        primary_tag: "Rock"
+    },
+
+    {
+        title: "Pretty", 
+        artist: "The Weeknd", 
+        lyrics: , 
+        primary_tag: "R&B"
+    },
+
+    {
+        title: "Redbone", 
+        artist: "Childish Gambino", 
+        lyrics: , 
+        primary_tag: "R&B"
+    },
+
+    {
+        title: "What's the Use?", 
+        artist: "Mac Miller", 
+        lyrics: , 
+        primary_tag: "Hip-Hop" 
+    },
+
+    {
+        title: "The Climb Back", 
+        artist: "J. Cole", 
+        lyrics: , 
+        primary_tag: "Hip-Hop"
+    },
+
+    {
+        title: "Ring Ring", 
+        artist: "Juice WRLD", 
+        lyrics: , 
+        primary_tag: "Hip-Hop"
+    },
+
+    {
+        title: "Doin' It Right (feat. Panda Bear)", 
+        artist: "Daft Punk", 
+        lyrics: , 
+        primary_tag: "Electronic"
+    },
+
+    {
+        title: "Headlines", 
+        artist: "Drake", 
+        lyrics: , 
+        primary_tag: "Hip-Hop"
+    },
+
+])
