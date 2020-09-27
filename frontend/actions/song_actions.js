187ae116555add2128ad64794eef7ac42f8ddf62
Drew <drewwebs@gmail.com>
2020-09-27 10:38:53 -0400

    song api util added

---
+++ frontend/actions/song_actions.js
@@ -0,0 +1,34 @@
+import * as SongUtil from '../util/song_util';
+
+export const RECEIVE_ALL_SONGS = 'RECEIVE_ALL_SONGS';
+export const RECEIVE_SONG = "RECEIVE_SONG";
+
+export const receiveSong = song => ({
+    type: RECEIVE_SONG,
+    song
+});
+
+export const receiveAllSongs = songs => ({
+    type: RECEIVE_ALL_SONGS,
+    songs
+});
+
+export const fetchSong = songId => dispatch => (
+    SongUtil.fetchSong(songId)
+    .then( song => dispatch(receiveSong(song)))
+);
+
+export const fetchAllSongs = filters => dispatch => (
+    SongUtil.fetchAllSongs(filters)
+    .then( songs => dispatch(receiveAllSongs(song)))
+);
+
+export const createSong = song => dispatch => (
+    SongUtil.createSong(song)
+    .then( song => dispatch(receiveSong(song)))
+);
+
+export const updateSong = song => dispatch => (
+    SongUtil.updateSong(song)
+    .then( song => dispatch(receiveSong(song)))
+);
---
+++ frontend/util/song_util.js
@@ -0,0 +1,29 @@
+export const fetchSong = songId => (
+    $.ajax({
+        method: 'GET',
+        url: `api/songs/${songId}`,
+    })
+);
+
+export const fetchAllSongs = filters => (
+    $.ajax({
+        method: 'GET',
+        url: 'api/songs',
+        data: filters
+    })
+);
+
+export const createSong = song => (
+    $.ajax({
+        method: 'POST',
+        url: 'api/songs',
+        data: song
+    })
+);
+
+export const updateSong = song => (
+    $.ajax({
+        method: 'PATCH',
+        url: `api/songs/${song.id}`
+    })
+);
