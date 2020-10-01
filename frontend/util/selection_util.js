e7ed8a0007b483a9a75284e32021601a6e1fe615
Drew <drewwebs@gmail.com>
2020-10-01 18:13:40 -0400

    Making sure migrations are here

--- frontend/util/selection_util.js
+++ frontend/util/selection_util.js
@@ -1,7 +1,15 @@
+const getLineNumber = (node) => {
+    if (node.previousElementSibling.nodeName === "A") {
+        return getLineNumber(node.previousElementSibling);
+    } else {
+        return node.previousElementSibling.dataset["sourcepos"].split(":")[0];
+    }
+};
+
 export default (selection) => {
     return {
-        line_start: selection.anchorNode.previousElementSibling.dataset["sourcepos"].split(":")[0],
-        line_end: selection.focusNode.previousElementSibling.dataset["sourcepos"].split(":")[0],
+        line_start: getLineNumber(selection.anchorNode),
+        line_end: getLineNumber(selection.focusNode),
         start_pos: selection.anchorOffset,
         end_pos: selection.focusOffset
     };
