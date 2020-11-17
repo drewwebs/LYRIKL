04fde2954629c157380564b04a8c32c4784165b2
Drew <drewwebs@gmail.com>
2020-11-17 14:51:52 -0500

    Fixed finish line/drown format

--- db/seeds.rb
+++ db/seeds.rb
@@ -1388,147 +1388,147 @@ Your tattoos, they're gonna fade",
         title: "Finish Line / Drown",
         artist: "Chance the Rapper feat. T‐Pain, Kirk Franklin, Eryn Allen Kane & Noname",
         lyrics: "
-[Part I: \"Finish Line\"]
-
-[Intro: Eryn Allen Kane & Chance The Rapper]
-Do-do-do-do-do-do-do
-Do-do-do-do-do-do-do
-Do-do-do-do-do-do-do
-Do-do-do-do-do
-And we back, and we back, and we back, and we back
-Do-do-do-do-do-do-do
-Do-do-do-do-do-do-do
-Do-do-do-do-do-do-do
-Do-do-do-do-do
-And we back, and we back, and we back
-
-[Verse 1: Chance The Rapper]
-They ain't teaching taxes in school
-It don't even matter, I was acting a fool
-But who would think the raps would turn into racks?
-Don't matter, matter fact, it could happen to you
-Scars on my head, I'm the boy who lived
-The boy love playing when the boy too sick
-Reclining on a prayer, I'm declining to help
-I've been lying to my body can't rely on myself, oh no
-Last year got addicted to Xans
-Started forgetting my name and started missing my chance
-L.A. for four months, end up leaving right back
-I'm in love with my city, bitch, I sleep in my hat, uh
-I felt hogtied ever since my dog died
-He lived to 84, damn, that's a long ride
-I know he up there, he just sit and he wait
-I'll be racing up the stairs, I'ma get to the gate singing
-
-[Bridge: Eryn Allen Kane]
-Do-do-do-do-do-do-do
-Do-do-do-do-do-do-do
-Do-do-do-do-do-do-do
-Do-do-do-do-do
-Do-do-do-do-do-do-do
-Do-do-do-do-do-do-do
-Do-do-do-do-do-do-do
-Do-do-do-do-do
-
-[Chorus: T-Pain]
-Oh, all my days
-I prayed and prayed
-And now I see the finish line
-Oh, I'm gonna finish mine, yeah
-All my days
-I prayed and prayed
-And now I see the finish line
-Gonna finish mine, yeah-eah
-
-[Verse 2: Chance The Rapper]
-Hey! Gimme the water, gimme the water
-I need the kind from Space Jam
-Get this money, get this budget, KOK to day camp
-Me and my girl plan to stay to the end
-Hope there never come a day where we be better as friends
-We in a marathon we could build a marriage on
-Arguments as parents digging deeper than a baritone
-I've been getting blocked just trying to make songs with friends
-Labels told me to my face that they own my friends
-I got to pray, I got to pray, like Hammer after \"2 Legit\"
-I got the power, I could poke Lucifer with crucifix
-I cannot scrape om stupid shit, I stand up like I'm Ludacris
-I know some folks that talk so much
-You'd think they drive an Uber whip, damn
-Queen said, \"Why we in a queen bed?\"
-I said, \"It's yours, don't worry, lil' beanhead\"
-I'm just here to catch my breath, I got a world tattoo
-She said, \"Cool, just bring me some food\"
-
-[Chorus: T-Pain]
-Oh, all my days
-I prayed and prayed
-And now I see the finish line
-Oh, I'm gonna finish mine, yeah
-All my days
-I prayed and prayed
-And now I see the finish line
-I'm gonna finish mine, yeah-eah
-
-[Bridge: Eryn Allen Kane]
-Do-do-do-do-do-do-do
-Do-do-do-do-do-do-do
-Do-do-do-do-do-do-do
-Do-do-do-do-do
-Do-do-do-do-do-do-do
-Do-do-do-do-do-do-do
-Do-do-do-do-do-do-do
-Do-do-do-do-do
-
-[Part II: \"Drown\"]
-
-[Intro: Eryn Allen Kane]
-The water may be deeper than it's ever been
-
-[Verse 1: Noname]
-Lord rain down on me so I can move on water
-Like children at the altar, like God inside my house
-I love you, I love you, you looking holy like Mama
-You made a church out of feathers, so when she fly to the Father
-She know the choir gon' follow and all the offering paid
-She gave my name away to your holy house
-She like my blessings in disguise
-She like her Jesus mountain high
-So He can watch her lonely child, I know my God
-I know my God seen His breaks and His edges
-Are jagged for giving that pain to His city in gold
-Like everything is everything
-Like all them days He prayed with me
-Like emptiness was tamed in me
-And all that was left was His love
-And all that was left was His love
-And all that was left was His love
-And all that was left was His love
-
-[Chorus: Choir]
-Never, never drown
-The water may be deeper than it's ever been
-Never drown
-The water may be deeper than it's ever been
-Never drown
-
-[Verse 2: Choir & Kirk Franklin]
-Never, never, never (Chance, let me in, come on)
-This thirst in my soul (There's a thirst in my soul, it tell me)
-Where else can I go? (Where?) but You? (Come on)
-I may cry a river (I may cry a thousand tears, but)
-But You take each drop and You wash (You wash me new)
-Me new (People sing)
-This water is deep (Yes, yes and I need You to)
-Jesus, rescue me (Come on, save me, I need You to)
-Take me to Your mountain (Hallelujah)
-So someday Chicago will be free (Someday, we'll all be free)
-Be free (Say)
-
-[Chorus: Choir]
-The water may be deeper than it's ever been
-Never drown
-The water may be deeper than it's ever been
+[Part I: \"Finish Line\"]  
+  
+[Intro: Eryn Allen Kane & Chance The Rapper]  
+Do-do-do-do-do-do-do  
+Do-do-do-do-do-do-do  
+Do-do-do-do-do-do-do  
+Do-do-do-do-do  
+And we back, and we back, and we back, and we back  
+Do-do-do-do-do-do-do  
+Do-do-do-do-do-do-do  
+Do-do-do-do-do-do-do  
+Do-do-do-do-do  
+And we back, and we back, and we back  
+  
+[Verse 1: Chance The Rapper]  
+They ain't teaching taxes in school  
+It don't even matter, I was acting a fool  
+But who would think the raps would turn into racks?  
+Don't matter, matter fact, it could happen to you  
+Scars on my head, I'm the boy who lived  
+The boy love playing when the boy too sick  
+Reclining on a prayer, I'm declining to help  
+I've been lying to my body can't rely on myself, oh no  
+Last year got addicted to Xans  
+Started forgetting my name and started missing my chance  
+L.A. for four months, end up leaving right back  
+I'm in love with my city, bitch, I sleep in my hat, uh  
+I felt hogtied ever since my dog died  
+He lived to 84, damn, that's a long ride  
+I know he up there, he just sit and he wait  
+I'll be racing up the stairs, I'ma get to the gate singing  
+  
+[Bridge: Eryn Allen Kane]  
+Do-do-do-do-do-do-do  
+Do-do-do-do-do-do-do  
+Do-do-do-do-do-do-do  
+Do-do-do-do-do  
+Do-do-do-do-do-do-do  
+Do-do-do-do-do-do-do  
+Do-do-do-do-do-do-do  
+Do-do-do-do-do  
+  
+[Chorus: T-Pain]  
+Oh, all my days  
+I prayed and prayed  
+And now I see the finish line  
+Oh, I'm gonna finish mine, yeah  
+All my days  
+I prayed and prayed  
+And now I see the finish line  
+Gonna finish mine, yeah-eah  
+  
+[Verse 2: Chance The Rapper]  
+Hey! Gimme the water, gimme the water  
+I need the kind from Space Jam  
+Get this money, get this budget, KOK to day camp  
+Me and my girl plan to stay to the end  
+Hope there never come a day where we be better as friends  
+We in a marathon we could build a marriage on  
+Arguments as parents digging deeper than a baritone  
+I've been getting blocked just trying to make songs with friends  
+Labels told me to my face that they own my friends  
+I got to pray, I got to pray, like Hammer after \"2 Legit\"  
+I got the power, I could poke Lucifer with crucifix  
+I cannot scrape om stupid shit, I stand up like I'm Ludacris  
+I know some folks that talk so much  
+You'd think they drive an Uber whip, damn  
+Queen said, \"Why we in a queen bed?\"  
+I said, \"It's yours, don't worry, lil' beanhead\"  
+I'm just here to catch my breath, I got a world tattoo  
+She said, \"Cool, just bring me some food\"  
+  
+[Chorus: T-Pain]  
+Oh, all my days  
+I prayed and prayed  
+And now I see the finish line  
+Oh, I'm gonna finish mine, yeah  
+All my days  
+I prayed and prayed  
+And now I see the finish line  
+I'm gonna finish mine, yeah-eah  
+  
+[Bridge: Eryn Allen Kane]  
+Do-do-do-do-do-do-do  
+Do-do-do-do-do-do-do  
+Do-do-do-do-do-do-do  
+Do-do-do-do-do  
+Do-do-do-do-do-do-do  
+Do-do-do-do-do-do-do  
+Do-do-do-do-do-do-do  
+Do-do-do-do-do  
+  
+[Part II: \"Drown\"]  
+  
+[Intro: Eryn Allen Kane]  
+The water may be deeper than it's ever been  
+  
+[Verse 1: Noname]  
+Lord rain down on me so I can move on water  
+Like children at the altar, like God inside my house  
+I love you, I love you, you looking holy like Mama  
+You made a church out of feathers, so when she fly to the Father  
+She know the choir gon' follow and all the offering paid  
+She gave my name away to your holy house  
+She like my blessings in disguise  
+She like her Jesus mountain high  
+So He can watch her lonely child, I know my God  
+I know my God seen His breaks and His edges  
+Are jagged for giving that pain to His city in gold  
+Like everything is everything  
+Like all them days He prayed with me  
+Like emptiness was tamed in me  
+And all that was left was His love  
+And all that was left was His love  
+And all that was left was His love  
+And all that was left was His love  
+  
+[Chorus: Choir]  
+Never, never drown  
+The water may be deeper than it's ever been  
+Never drown  
+The water may be deeper than it's ever been  
+Never drown  
+  
+[Verse 2: Choir & Kirk Franklin]  
+Never, never, never (Chance, let me in, come on)  
+This thirst in my soul (There's a thirst in my soul, it tell me)  
+Where else can I go? (Where?) but You? (Come on)  
+I may cry a river (I may cry a thousand tears, but)  
+But You take each drop and You wash (You wash me new)  
+Me new (People sing)  
+This water is deep (Yes, yes and I need You to)  
+Jesus, rescue me (Come on, save me, I need You to)  
+Take me to Your mountain (Hallelujah)  
+So someday Chicago will be free (Someday, we'll all be free)  
+Be free (Say)  
+  
+[Chorus: Choir]  
+The water may be deeper than it's ever been  
+Never drown  
+The water may be deeper than it's ever been  
 Never drown",
     primary_tag: "Rap"
     },
--- frontend/util/markdown_util.js
+++ frontend/util/markdown_util.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import SongShow from '../components/songs/song_show';
 
 export const referenceHandler = (reference) => {
     if (!reference.href) {
@@ -9,13 +7,8 @@ export const referenceHandler = (reference) => {
     return <a href={reference.$ref}>{reference.children}</a>
 }
 
-export const linkCreator = (reference) => {
-    const parsed = []
-    reference.children.forEach( child => child.type === "br" ? parsed.push("<br />") : parsed.push(child.props.value) )
-    
-    return (
-      <button id={`${reference.href}`} className="annotation-element"> 
-        {reference.children.map( child => child.type === "br" ? <br /> : child.props.value )}
-      </button>
-    );
-}
+export const linkCreator = (reference) => (
+    <button id={`${reference.href}`} className="annotation-element"> 
+      {reference.children.map( (child) => child.type === "br" ? <br /> : child.props.value )}
+    </button>
+)
