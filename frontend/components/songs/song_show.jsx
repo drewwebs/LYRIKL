76c89b1e36ac6c04b67e48e0b7739380fcbd5b34
Drew <drewwebs@gmail.com>
2020-11-29 21:55:52 -0500

    small props fix

--- frontend/components/songs/song_show.jsx
+++ frontend/components/songs/song_show.jsx
@@ -118,7 +118,7 @@ export default (props) => {
                                                                 selection={selection}
                                                                 songId={props.song.id}
                                                                 yOffset={yOffset} 
-                                                                addAnnotation={setAnnotation}
+                                                                setAnnotation={setAnnotation}
                                                             /> : 
                                                             <EditAnnotation
                                                                 setAnnotationForm={setAnnotationForm}
