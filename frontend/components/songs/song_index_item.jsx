4732f7eb3f013d4a058f24bf1b74a28459118591
Drew <drewwebs@gmail.com>
2020-11-29 21:21:37 -0500

    Fixed charts styling on smaller screens

--- frontend/components/songs/song_index_item.jsx
+++ frontend/components/songs/song_index_item.jsx
@@ -5,8 +5,10 @@ import {Link} from 'react-router-dom';
 export default (props) => (
     (
         <Link className="charts-song" to={`/songs/${props.song.id}`}>
-            <img className="charts-song-cover" src={`${props.song.image_url}`} alt=""/>
-            <p className="charts-song-title">{props.song.title}</p>
+            <div className="charts-song-details">
+                <img className="charts-song-cover" src={`${props.song.image_url}`} alt="" />
+                <p className="charts-song-title">{props.song.title}</p>
+            </div>
             <p className="charts-song-artist">{props.song.artist}</p>
             <p className="charts-song-view-count">
                 <span className={props.song.fire ? "" : "hidden"}>
